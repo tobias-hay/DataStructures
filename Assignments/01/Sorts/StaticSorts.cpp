267be6863771a2b97333ffaa8d6d0561f4cd50b1
Tobias <61364477+tobias-hay@users.noreply.github.com>
2026-09-15 19:48:18 -0400

    feat: bubble

--- Assignments/01/Sorts/StaticSorts.cpp
+++ Assignments/01/Sorts/StaticSorts.cpp
@@ -1,2 +1,13 @@
 
 #include "StaticSorts.h"
+
+#include "../Utils/Utils.h"
+
+void StaticSorts::bubble(std::vector<int> &arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = 0; j < arr.size() - i - 1; j++) {
+            if (arr[j] > arr[j + 1])
+                Utils::swap(arr, j, j + 1);
+        }
+    }
+}
--- Assignments/01/main.cpp
+++ Assignments/01/main.cpp
@@ -1,10 +1,18 @@
 #include <vector>
+#include <iostream>
 
+#include "Sorts/StaticSorts.h"
 #include "Utils/Utils.h"
 
 int main() {
-    std::vector<int> arr = Utils::generateArray(10, 20);
+    std::vector<int> arr = Utils::generateArray(20, 20);
 
+    std::cout << "Unsorted: ";
+    Utils::print(arr);
+
+    StaticSorts::bubble(arr);
+
+    std::cout << "Sorted: ";
     Utils::print(arr);
 
     return 0;
